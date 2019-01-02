2bd6947e0f5268b8dc5ffea1b9b68feecf5ae10f
yuliailchyshyn <43512276+yuliailchyshyn@users.noreply.github.com>
2019-01-02 20:05:27 +0200

    Update Створення таблиці .sql

--- Створення таблиці .sql
+++ Створення таблиці .sql
@@ -9,10 +9,7 @@ CREATE TABLE [dbo].[Авіаперевізники](
 	[Місто вильоту] [nvarchar](30) NOT NULL,
 	[Рейси] [nvarchar](max) NULL,
  CONSTRAINT [PK_Авіаперевізники] PRIMARY KEY CLUSTERED 
-(
-	[Авіаперевізник] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
 
 GO
 
@@ -24,12 +21,7 @@ CREATE TABLE [dbo].[Замовлення](
 	[Кількість путівок] [int] NOT NULL,
 	[Авіаперевізник] [nvarchar](30) NOT NULL,
 	[Дата оплати] [date] NULL,
- CONSTRAINT [PK_Замовлення] PRIMARY KEY CLUSTERED 
-(
-	[Ідентифікаційний код клієнта] ASC,
-	[Код туру] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+ CONSTRAINT [PK_Замовлення] PRIMARY KEY  [Ідентифікаційний код клієнта] ASC, [Код туру] ASC)
 
 GO
 
@@ -39,28 +31,14 @@ CREATE TABLE [dbo].[Клієнти](
 	[Прізвище] [nvarchar](50) NOT NULL,
 	[Ім'я] [nvarchar](20) NOT NULL,
 	[По батькові] [nvarchar](50) NULL,
- CONSTRAINT [PK_Клієнти] PRIMARY KEY CLUSTERED 
-(
-	[Ідентифікаційний код клієнта] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
+ CONSTRAINT [PK_Клієнти] PRIMARY KEY CLUSTERED [Ідентифікаційний код клієнта] ASC)
 GO
 
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[Напрямки](
 	[Код напрямку] [int] NOT NULL,
 	[Країна] [nvarchar](30) NOT NULL,
 	[Місто] [nvarchar](30) NOT NULL,
- CONSTRAINT [PK_Напрямки] PRIMARY KEY CLUSTERED 
-(
-	[Код напрямку] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
+ CONSTRAINT [PK_Напрямки] PRIMARY KEY CLUSTERED [Код напрямку] ASC)
 GO
 
 CREATE TABLE [dbo].[Тури](
@@ -68,31 +46,34 @@ CREATE TABLE [dbo].[Тури](
 	[Код напрямку] [int] NOT NULL,
 	[Тривалість туру] [int] NOT NULL,
 	[Ціна туру] [money] NOT NULL,
- CONSTRAINT [PK_Тури] PRIMARY KEY CLUSTERED 
-(
-	[Код туру] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
+ CONSTRAINT [PK_Тури] PRIMARY KEY CLUSTERED [Код туру] ASC)
 GO
+	 
 ALTER TABLE [dbo].[Замовлення]  WITH CHECK ADD  CONSTRAINT [FK_Замовлення_Авіаперевізники] FOREIGN KEY([Авіаперевізник])
 REFERENCES [dbo].[Авіаперевізники] ([Авіаперевізник])
 GO
+	 
 ALTER TABLE [dbo].[Замовлення] CHECK CONSTRAINT [FK_Замовлення_Авіаперевізники]
 GO
+	 
 ALTER TABLE [dbo].[Замовлення]  WITH CHECK ADD  CONSTRAINT [FK_Замовлення_Клієнти] FOREIGN KEY([Ідентифікаційний код клієнта])
 REFERENCES [dbo].[Клієнти] ([Ідентифікаційний код клієнта])
 GO
+	 
 ALTER TABLE [dbo].[Замовлення] CHECK CONSTRAINT [FK_Замовлення_Клієнти]
 GO
+	 
 ALTER TABLE [dbo].[Замовлення]  WITH CHECK ADD  CONSTRAINT [FK_Замовлення_Тури] FOREIGN KEY([Код туру])
 REFERENCES [dbo].[Тури] ([Код туру])
 GO
+	 
 ALTER TABLE [dbo].[Замовлення] CHECK CONSTRAINT [FK_Замовлення_Тури]
 GO
+	 
 ALTER TABLE [dbo].[Тури]  WITH CHECK ADD  CONSTRAINT [FK_Тури_Напрямки] FOREIGN KEY([Код напрямку])
 REFERENCES [dbo].[Напрямки] ([Код напрямку])
 GO
+	 
 ALTER TABLE [dbo].[Тури] CHECK CONSTRAINT [FK_Тури_Напрямки]
 GO
 
